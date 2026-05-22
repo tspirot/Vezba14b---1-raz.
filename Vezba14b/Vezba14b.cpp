3f0da5750bb126aa04e115eb8533dee65b3d1e8e
blagojevicboban <boban.blagojevic@tsp.edu.rs>
2026-05-22 10:56:25 +0200

    Add project files.

---
+++ Vezba14b/Vezba14b.cpp
@@ -0,0 +1,27 @@
+// Vezba14b.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// DO petlja - pietlja sa uslovom na dnu
+// Korisnik unosi cene artikala dok ne unese 0 
+// (dok ne unese n)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	float cena, ukupno = 0;
+	char dalje;
+	i = 0;
+	do
+	{
+		printf("Unesi cenu %d. artikla:\n", i+1);
+		scanf("%f", &cena);
+		ukupno += cena;
+		i++;
+		printf("Da li hoces dalje (d/n):\n");
+		scanf(" %c", &dalje);
+	}
+	while (dalje == 'd');
+	printf("Uneli ste %d cena.\n", i);
+	printf("Ukupna cena je %.2f.\n", ukupno); 
+	printf("Prosecna cena je %.2f.\n", ukupno / i);
+}
